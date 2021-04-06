0c72f99dce854f8f961223a74a0479952dfd52e7
Linojones <flp_l@hotmail.com>
2021-04-06 12:26:08 -0300

    Add movimento para peça (BISPO)
    Adicionando os movimentos da peça bispo e inserindo no tabuleiro.

--- src/xadrez/pecas/Bispo.java
+++ src/xadrez/pecas/Bispo.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogoTabuleiro.Posicao;
 import jogoTabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -12,14 +13,56 @@ public class Bispo extends PecaXadrez {
 	}
 	
 	@Override
+	public boolean[][] movimentosPossiveis() {
+		boolean[][] mat = new boolean [getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao p = new Posicao (0,0);
+		
+		// Definindo os movimentos possives da peça.
+		
+		// Noroeste
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Nordeste
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Sudeste
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Sudoeste
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+				
+		return mat;
+	}
+	
+	@Override
 	public String toString() {
 		return "B";
-	}
-
-	@Override
-	public boolean[][] movimentosPossiveis() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
