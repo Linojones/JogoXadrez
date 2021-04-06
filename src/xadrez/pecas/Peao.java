7273e5c2916139d825dedb11e07cf5119425cb1b
Linojones <flp_l@hotmail.com>
2021-04-06 11:58:05 -0300

    Add movimento para peça (PEÃO)
    Adicionando os movimentos da peça peão e inserindo no tabuleiro.

--- src/xadrez/pecas/Peao.java
+++ src/xadrez/pecas/Peao.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogoTabuleiro.Posicao;
 import jogoTabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -12,14 +13,62 @@ public class Peao extends PecaXadrez {
 	}
 	
 	@Override
+	public boolean[][] movimentosPossiveis() {
+		boolean[][] mat = new boolean [getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao p = new Posicao (0,0);
+		
+		// Movimento do peão (A frente)
+		if (getCor() == Cor.BRANCO) {
+			p.definirValores(posicao.getLinha() - 1, posicao.getColuna());
+			if (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+			// Primeiro movimento, andando 2 casas do tabuleiro
+			p.definirValores(posicao.getLinha() - 2, posicao.getColuna());
+			Posicao p2 = new Posicao(posicao.getLinha() - 1, posicao.getColuna());
+			if (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p) && getTabuleiro().posicaoExiste(p2) && !getTabuleiro().temUmaPeca(p2) && getContarMovimento() == 0) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+			// Capturando peca adversaria esquerda (Noroeste)
+			p.definirValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+			if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+			// Capturando peca adversaria direita (Nordeste)
+			p.definirValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+			if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+		}
+		else {
+			p.definirValores(posicao.getLinha() + 1, posicao.getColuna());
+			if (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+			// Primeiro movimento, andando 2 casas do tabuleiro
+			p.definirValores(posicao.getLinha() + 2, posicao.getColuna());
+			Posicao p2 = new Posicao(posicao.getLinha() + 1, posicao.getColuna());
+			if (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p) && getTabuleiro().posicaoExiste(p2) && !getTabuleiro().temUmaPeca(p2) && getContarMovimento() == 0) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+			// Capturando peca adversaria esquerda (Sudoeste)
+			p.definirValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+			if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+			// Capturando peca adversaria direita (Sudeste)
+			p.definirValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+			if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+				mat[p.getLinha()][p.getColuna()] = true;
+			}
+		}
+		return mat;
+	}
+	
+	@Override
 	public String toString() {
 		return "P";
-	}
-
-	@Override
-	public boolean[][] movimentosPossiveis() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
