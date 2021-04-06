1b382171fb4feb028cd4d6ec101f29f090da507e
Linojones <flp_l@hotmail.com>
2021-04-06 13:36:20 -0300

    Add movimento para peça (RAINHA)
    Adicionando os movimentos da peça rainha e inserindo no tabuleiro.

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -10,6 +10,7 @@ import jogoTabuleiro.Tabuleiro;
 import xadrez.pecas.Bispo;
 import xadrez.pecas.Cavalo;
 import xadrez.pecas.Peao;
+import xadrez.pecas.Rainha;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
 
@@ -198,6 +199,7 @@ public class PartidaXadrez {
 		colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('a', 2, new Peao(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('b', 2, new Peao(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('c', 2, new Peao(tabuleiro, Cor.BRANCO));
@@ -214,6 +216,7 @@ public class PartidaXadrez {
 		colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('d', 8, new Rainha(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('a', 7, new Peao(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('b', 7, new Peao(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('c', 7, new Peao(tabuleiro, Cor.PRETO));
--- src/xadrez/pecas/Rainha.java
+++ src/xadrez/pecas/Rainha.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogoTabuleiro.Posicao;
 import jogoTabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -12,14 +13,89 @@ public class Rainha extends PecaXadrez{
 	}
 	
 	@Override
+	public boolean[][] movimentosPossiveis() {
+boolean[][] mat = new boolean [getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao p = new Posicao (0,0);
+		
+		// Acima
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna());
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Abaixo
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna());
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Esquerda
+		p.definirValores(posicao.getLinha(), posicao.getColuna() - 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Direita
+		p.definirValores(posicao.getLinha(), posicao.getColuna() + 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Noroeste
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Nordeste
+		p.definirValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Sudeste
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		// Sudoeste
+		p.definirValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.definirValores(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+		}
+		return mat;
+	}
+	
+	@Override
 	public String toString() {
 		return "Q";
-	}
-
-	@Override
-	public boolean[][] movimentosPossiveis() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
